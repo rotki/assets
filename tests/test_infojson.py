45bc9deb99fee10ec0ea8c47f652a56c478953bb
Lefteris Karapetsas <lefteris@refu.co>
2021-04-17 22:05:00 +0200

    Add script to auto-populate info.json and also script to test it

---
+++ tests/test_infojson.py
@@ -0,0 +1,27 @@
+"""
+Small sanity checks
+
+TODO: Probably should turn this into part of a pytest suite
+"""
+import json
+from pathlib import Path
+
+
+root_dir = infojson = Path(__file__).parents[1]
+infojson = root_dir / 'updates' / 'info.json'
+assert infojson.is_file()
+
+with open(infojson) as f:
+    data = json.load(f)
+assert isinstance(data, dict)
+assert 'latest' in data
+updates = data['updates']
+for update, update_data in updates.items():
+    assert update_data['min_schema_version'] <= update_data['max_schema_version']
+    update_dir = root_dir / 'updates' / update
+    assert update_dir.is_dir()
+    update_file = update_dir / 'updates.sql'
+    assert update_file.is_file()
+    with open(update_file) as f:
+        changes = sum(1 for line in f) / 2
+    assert changes == update_data['changes']

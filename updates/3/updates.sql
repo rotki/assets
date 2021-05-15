50c6a7d98da8b47bb3ee891ce12c2839c12b2579
Yabir Garcia <yabirg@protonmail.com>
2021-05-15 19:03:08 +0200

    Add assets for rotki v1.17.0
    Closes rotki/rotki#2907
    Closes rotki/rotki#2884
    Closes rotki/rotki#2882
    Closes rotki/rotki#2873
    Closes rotki/rotki#2825
    Closes rotki/rotki#2824
    Closes rotki/rotki#2790

---
+++ updates/3/updates.sql
@@ -0,0 +1,16 @@
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xD0660cD418a64a1d44E9214ad8e459324D8157f1", 12, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xD0660cD418a64a1d44E9214ad8e459324D8157f1", "C", "Woofy", "WOOFY", 1620761276, NULL, "woofy", "WOOFY", "0xD0660cD418a64a1d44E9214ad8e459324D8157f1");
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("ICP", "W", "Internet Computer", "ICP", 1620673391, NULL, "internet-computer", "ICP", "ICP"); INSERT INTO common_asset_details(asset_id, forked) VALUES("ICP", NULL);
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("BAG", "W", "Baguette", "BAG", 1618757419, NULL, "baguette", "", "BAG"); INSERT INTO common_asset_details(asset_id, forked) VALUES("BAG", NULL);
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x066798d9ef0833ccc719076Dab77199eCbd178b0", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x066798d9ef0833ccc719076Dab77199eCbd178b0", "C", "SakeToken", "SAKE", 1599552685, NULL, "sake-token", "SAKE", "0x066798d9ef0833ccc719076Dab77199eCbd178b0");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xC4C2614E694cF534D407Ee49F8E44D125E4681c4", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xC4C2614E694cF534D407Ee49F8E44D125E4681c4", "C", "Chain Games", "CHAIN", 1597748317, NULL, "chain-games", "CHAIN", "0xC4C2614E694cF534D407Ee49F8E44D125E4681c4");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x536381a8628dBcC8C70aC9A30A7258442eAb4c92", 8, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x536381a8628dBcC8C70aC9A30A7258442eAb4c92", "C", "Pantos Token", "PAN", 1538478818, NULL, "pantos", "PAN", "0x536381a8628dBcC8C70aC9A30A7258442eAb4c92");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xDd1Ad9A21Ce722C151A836373baBe42c868cE9a4", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xDd1Ad9A21Ce722C151A836373baBe42c868cE9a4", "C", "Universal Basic Income", "UBI", 1615393750, NULL, "universal-basic-income", "UBI", "0xDd1Ad9A21Ce722C151A836373baBe42c868cE9a4");
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("MHC", "W", "#MetaHash", "MHC", 1550624400, NULL, "metahash", "MHC", "MHC"); INSERT INTO common_asset_details(asset_id, forked) VALUES("MHC", NULL);
+*

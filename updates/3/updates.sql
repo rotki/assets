abf85110084a330d2fe20ae81e258a287b77e330
Yabir Garcia <yabirg@protonmail.com>
2021-05-17 00:17:54 +0200

    Add new kucoin assets

--- updates/3/updates.sql
+++ updates/3/updates.sql
@@ -18,3 +18,21 @@ INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xC477D038d5420
 *
 INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("ANC", "W", "Anchor Protocol", "ANC", 1615986013, NULL, "anchor-protocol", "ANC", "ANC"); INSERT INTO common_asset_details(asset_id, forked) VALUES("ANC", NULL);
 *
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xF59ae934f6fe444afC309586cC60a84a0F89Aaea", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xF59ae934f6fe444afC309586cC60a84a0F89Aaea", "C", "Polkadex", "PDEX", 1618504926, NULL, "polkadex", "PDEX", "0xF59ae934f6fe444afC309586cC60a84a0F89Aaea");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x9040e237C3bF18347bb00957Dc22167D0f2b999d", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x9040e237C3bF18347bb00957Dc22167D0f2b999d", "C", "Standard", "STND", 1619689390, NULL, "standard-protocol", "STND", "0x9040e237C3bF18347bb00957Dc22167D0f2b999d");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x60Eb57d085C59932d5fAa6c6026268A4386927d0", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x60Eb57d085C59932d5fAa6c6026268A4386927d0", "C", "LOCGame", "LOCG", 1619642983, NULL, "locgame", "LOCG", "0x60Eb57d085C59932d5fAa6c6026268A4386927d0");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x85f6eB2BD5a062f5F8560BE93FB7147e16c81472", 4, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x85f6eB2BD5a062f5F8560BE93FB7147e16c81472", "C", "Franklin", "FLy", 1610530262, NULL, "franklin", "", "0x85f6eB2BD5a062f5F8560BE93FB7147e16c81472");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86", "C", "Origin Dollar", "OUSD", 1600410737, NULL, "origin-dollar", "", "0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xaC0104Cca91D167873B8601d2e71EB3D4D8c33e0", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xaC0104Cca91D167873B8601d2e71EB3D4D8c33e0", "C", "Crowns", "CWS", 1612013846, NULL, "crowns", "CWS", "0xaC0104Cca91D167873B8601d2e71EB3D4D8c33e0");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xeEAA40B28A2d1b0B08f6f97bB1DD4B75316c6107", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xeEAA40B28A2d1b0B08f6f97bB1DD4B75316c6107", "C", "GOVI", "GOVI", 1608731186, NULL, "govi", "GOVI", "0xeEAA40B28A2d1b0B08f6f97bB1DD4B75316c6107");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x9F9c8ec3534c3cE16F928381372BfbFBFb9F4D24", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x9F9c8ec3534c3cE16F928381372BfbFBFb9F4D24", "C", "GraphLinq", "GLQ", 1615827809, NULL, "graphlinq-protocol", "GLQ", "0x9F9c8ec3534c3cE16F928381372BfbFBFb9F4D24");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x7825e833D495F3d1c28872415a4aee339D26AC88", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x7825e833D495F3d1c28872415a4aee339D26AC88", "C", "pTokens TLOS", "TLOS", 1613670383, NULL, "telos", "TLOS", "0x7825e833D495F3d1c28872415a4aee339D26AC88");
+*

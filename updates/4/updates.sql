4f018d39b453c970695c344cd300c48ce7b96f4a
Yabir Garcia <yabirg@protonmail.com>
2021-06-01 15:45:10 +0200

    Include assets from exchanges and coingecko updates

--- updates/4/updates.sql
+++ updates/4/updates.sql
@@ -10,3 +10,29 @@ INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xc5bDdf9843308
 *
 INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a", "C", "Yearn Compounding veCRV yVault", "yvBOOST", 1617828349, NULL, "yvboost", "YVBOOST", "0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a");
 *
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x6D0F5149c502faf215C89ab306ec3E50b15e2892", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x6D0F5149c502faf215C89ab306ec3E50b15e2892", "C", "Portion Token", "PRT", 1600396712, NULL, "portion", "", "0x6D0F5149c502faf215C89ab306ec3E50b15e2892");
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("MINA", "W", "Mina Protocol", "MINA", 1622513694, NULL, "mina-protocol", "MINA", "MINA"); INSERT INTO common_asset_details(asset_id, forked) VALUES("MINA", NULL);
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x038a68FF68c393373eC894015816e33Ad41BD564", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x038a68FF68c393373eC894015816e33Ad41BD564", "C", "Glitch", "GLCH", 1608173369, NULL, "glitch-protocol", "GLCH", "0x038a68FF68c393373eC894015816e33Ad41BD564");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x7659CE147D0e714454073a5dd7003544234b6Aa0", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x7659CE147D0e714454073a5dd7003544234b6Aa0", "C", "XCAD Token", "XCAD", 1620952873, NULL, "xcad-network", "XCAD", "0x7659CE147D0e714454073a5dd7003544234b6Aa0");
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("APL", "W", "Apollo", "APL", 1533607200, NULL, "apollo", "APL", "APL"); INSERT INTO common_asset_details(asset_id, forked) VALUES("APL", NULL);
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("VEED", "W", "VEED", "VEED", 1622439185, NULL, "veed", "", "VEED"); INSERT INTO common_asset_details(asset_id, forked) VALUES("VEED", NULL);
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x4B1E80cAC91e2216EEb63e29B957eB91Ae9C2Be8", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x4B1E80cAC91e2216EEb63e29B957eB91Ae9C2Be8", "C", "Jupiter", "JUP", 1597548805, NULL, "jupiter", "JUP", "0x4B1E80cAC91e2216EEb63e29B957eB91Ae9C2Be8");
+*
+UPDATE assets SET cryptocompare="MEDIA" WHERE identifier="MEDIA";/*Update crytocompare*/
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("MEDIA", "Y", "Media Network", "MEDIA", 1619799163, NULL, "media-network", "MEDIA", "MEDIA"); INSERT INTO common_asset_details(asset_id, forked) VALUES("MEDIA", NULL);
+UPDATE assets SET coingecko=NULL WHERE identifier="_ceth_0x4D9e23a3842fE7Eb7682B9725cF6c507C424A41B";/*Coingecko doesnt provide informationm anymore*/
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x4D9e23a3842fE7Eb7682B9725cF6c507C424A41B", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x4D9e23a3842fE7Eb7682B9725cF6c507C424A41B", "C", "CarBlock", "CAR", 1530154004, NULL, NULL, "CAR", "0x4D9e23a3842fE7Eb7682B9725cF6c507C424A41B");
+UPDATE assets SET coingecko="celletf" WHERE identifier="_ceth_0x9b62Ec1453cEa5Dde760AAf662048cA6eEB66E7f";/*Coingecko changed id*/
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x9b62Ec1453cEa5Dde760AAf662048cA6eEB66E7f", 2, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x9b62Ec1453cEa5Dde760AAf662048cA6eEB66E7f", "C", "Consensus Cell Network", "ECELL", 1566879537, NULL, "celletf", "ECELL", "0x9b62Ec1453cEa5Dde760AAf662048cA6eEB66E7f");
+UPDATE assets SET coingecko="uno-re" WHERE identifier="_ceth_0x474021845C4643113458ea4414bdb7fB74A01A77";/*Coingecko changed id*/
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x474021845C4643113458ea4414bdb7fB74A01A77", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x474021845C4643113458ea4414bdb7fB74A01A77", "C", "UnoRe", "UNO", 1619478432, NULL, "uno-re", "UNORE", "0x474021845C4643113458ea4414bdb7fB74A01A77");
+UPDATE assets SET coingecko="bitcny" WHERE identifier="BITCNY";/*Coingecko changed id*/
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("BITCNY", "G", "bitCNY", "BITCNY", 1411446262, NULL, "bitcny", "BITCNY", "BITCNY"); INSERT INTO common_asset_details(asset_id, forked) VALUES("BITCNY", NULL);
+UPDATE assets SET coingecko=NULL WHERE identifier="PIGGY";/*Coingecko changed id*/
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("PIGGY", "B", "Piggycoin", "PIGGY", 1433894400, NULL, NULL, "PIGGY", "PIGGY"); INSERT INTO common_asset_details(asset_id, forked) VALUES("PIGGY", NULL);

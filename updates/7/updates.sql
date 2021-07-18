1e09585f86dfaa50745de0e308cd9412c68fb6ed
Yabir Garcia <yabirg@protonmail.com>
2021-07-18 22:16:19 +0200

    Adds AAVEUP, AAVEDOWN and Capsule Coin
    Closes rotki/rotki#3213

---
+++ updates/7/updates.sql
@@ -0,0 +1,6 @@
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x03Be5C903c727Ee2C8C4e9bc0AcC860Cca4715e2", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x03Be5C903c727Ee2C8C4e9bc0AcC860Cca4715e2", "C", "Capsule Coin", "CAPS", 1614363963, NULL, "coin-capsule", "CAPS", "0x03Be5C903c727Ee2C8C4e9bc0AcC860Cca4715e2");
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("AAVEUP", "W", "AAVEUP", "AAVEUP", 1606374000, NULL, "", "AAVEUP", "AAVEUP"); INSERT INTO common_asset_details(asset_id, forked) VALUES("AAVEUP", NULL);
+*
+INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("AAVEDOWN", "W", "AAVEDOWN", "AAVEDOWN", 1606374000, NULL, "", "AAVEDOWN", "AAVEDOWN"); INSERT INTO common_asset_details(asset_id, forked) VALUES("AAVEDOWN", NULL);
+*

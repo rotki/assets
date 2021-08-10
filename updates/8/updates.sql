24c42ec6fd354a59b35df2bef754f6bda011f028
defi-caratacus <defi.caratacus@tutanota.com>
2021-08-10 11:55:27 +0200

    Add NKN token
    https://www.coingecko.com/en/coins/nkn

---
+++ updates/8/updates.sql
@@ -0,0 +1,2 @@
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x5Cf04716BA20127F1E2297AdDCf4B5035000c9eb", 18, NULL); INSERT INTO assets(identifier, type, name, symbol,started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x5Cf04716BA20127F1E2297AdDCf4B5035000c9eb", "C", "NKN", "NKN", 1546084520, NULL, "nkn", "NKN", "0x5Cf04716BA20127F1E2297AdDCf4B5035000c9eb");
+*

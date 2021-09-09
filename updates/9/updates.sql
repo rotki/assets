4c833f2e39eeb1aba4f7eaaf4def67eedd198377
Yabir Benchakhtir <yabirg@protonmail.com>
2021-09-09 16:42:50 +0200

    Add discord assets

--- updates/9/updates.sql
+++ updates/9/updates.sql
@@ -1,2 +1,10 @@
 INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("DKK", "A", "Danish krone", "DKK", NULL, NULL, NULL, "", "DKK"); INSERT INTO common_asset_details(asset_id, forked) VALUES("DKK", NULL);
 *
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x92D6C1e31e14520e676a687F0a93788B716BEff5", 18, NULL); INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x92D6C1e31e14520e676a687F0a93788B716BEff5", "C", "dYdX", "DYDX", 1626053832, NULL, "dydx", "", "0x92D6C1e31e14520e676a687F0a93788B716BEff5");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x993864E43Caa7F7F12953AD6fEb1d1Ca635B875F", 18, NULL); INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x993864E43Caa7F7F12953AD6fEb1d1Ca635B875F", "C", "Singularity Dao", "SDAO", 1620379310, NULL, "singularitydao", "", "0x993864E43Caa7F7F12953AD6fEb1d1Ca635B875F");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0x1190074795DAD0E61b61270De48e108427f8f817", 9, NULL); INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0x1190074795DAD0E61b61270De48e108427f8f817", "C", "Coin Merge", "CMERGE", 1626393529, NULL, "coinmerge", "CMERGE", "0x1190074795DAD0E61b61270De48e108427f8f817");
+*
+INSERT INTO ethereum_tokens(address, decimals, protocol) VALUES("0xB4EaF48bD7f72356e1019C157e91b81A1C541073", 9, NULL); INSERT INTO assets(identifier, type, name, symbol, started, swapped_for, coingecko, cryptocompare, details_reference) VALUES("_ceth_0xB4EaF48bD7f72356e1019C157e91b81A1C541073", "C", "ApeBullInu", "ABI", 1626991974, NULL, "apebullinu", "ABI", "0xB4EaF48bD7f72356e1019C157e91b81A1C541073");
+*

41074f7c96580ff602eabc823f43500a225935f8
Yabir Benchakhtir <yabirg@protonmail.com>
2023-05-08 17:59:50 +0200

    Assets for 1.28

---
+++ updates/18/asset_collections_mappings_updates.sql
@@ -0,0 +1,42 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (216, "eip155:42161/erc20:0x912ce59144191c1204e64559fe8253a0e49e6548");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (216, "eip155:1/erc20:0xb50721bcf8d664c30412cfbc6cf7a15145234ad1");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:1/erc20:0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:42161/erc20:0x5979d7b546e38e414f7e9822514be443a4800529");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:10/erc20:0x1f32b1c2345538c0c6f582fcb022739c4a194ebb");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:1/erc20:0x5e8422345238f34275888049021821e8e08caa1f");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:42161/erc20:0x178412e79c25968a32e89b11f63b33f733770c2a");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:10/erc20:0x6806411765af15bddd26f8f544a34cc40cb9838b");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:56/erc20:0x64048a7eecf3a2f1ba9e144aac3d7db6e58f555e");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:137/erc20:0xee327f889d5947c1dc1934bb208a1e792f953e96");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:250/erc20:0x9e73f99ee061c8807f69f9c6ccc44ea3d8c373ee");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:1/erc20:0xac3e018457b222d93114458476f3e3416abbe38f");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:42161/erc20:0x95ab45875cffdba1e5f451b950bc2e42c0053f39");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:10/erc20:0x484c2d6e3cdd945a8b2df735e079178c1036578c");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:56/erc20:0x3cd55356433c89e50dc51ab07ee0fa0a95623d53");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:137/erc20:0x6d1fdbb266fcc09a16a22016369210a15bb95761");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (219, "eip155:250/erc20:0xb90ccd563918ff900928dc529aa01046795ccb4a");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (128, "eip155:42161/erc20:0x93b346b6bc2548da6a1e7d98e9a421b42541425b");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (128, "eip155:10/erc20:0xc40f949f8a4e094d1b49a23ea9241d289b7b2819");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (220, "eip155:1/erc20:0x5fab9761d60419c9eeebe3915a8fa1ed7e8d2e1b");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (220, "eip155:137/erc20:0xe261d618a959afffd53168cd07d12e37b26761db");
+*
---
+++ updates/18/asset_collections_updates.sql
@@ -0,0 +1,10 @@
+INSERT INTO asset_collections(id, name, symbol) VALUES (216, "Arbitrum", "ARB");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (217, "Wrapped liquid staked Ether 2.0", "wstETH");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (218, "Frax Ether", "frxETH");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (219, "Staked Frax Ether", "sfrxETH");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (220, "Dimo", "DIMO");
+*

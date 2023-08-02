3b5582d300ac4e45ff739d1e1236e3ff3f8bea35
Yabir Benchakhtir <yabirg@protonmail.com>
2023-08-02 20:31:17 +0200

    Add assets for 1.30

---
+++ updates/20/asset_collections_mappings_updates.sql
@@ -0,0 +1,12 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (231, "eip155:1/erc20:0x163f8c2467924be0ae7b5347228cabf260318753");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (231, "eip155:10/erc20:0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (232, "eip155:137/erc20:0x4b85a666dec7c959e88b97814e46113601b07e57");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (232, "eip155:56/erc20:0x4b85a666dec7c959e88b97814e46113601b07e57");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (233, "eip155:1/erc20:0x6731827cb6879a2091ce3ab3423f7bf20539b579");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (233, "eip155:137/erc20:0x47c52f93a359db9f4509005cf982dfc440790561");
+*
---
+++ updates/20/asset_collections_updates.sql
@@ -0,0 +1,6 @@
+INSERT INTO asset_collections(id, name, symbol) VALUES (231, "Worldcoin", "WLD");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (232, "GoCrypto", "GOC");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (233, "Empower Token", "MPWR");
+*

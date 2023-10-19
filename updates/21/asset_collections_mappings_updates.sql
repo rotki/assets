92997bd6f77804281a202d713958faf2534d6917
Yabir Benchakhtir <yabirg@protonmail.com>
2023-10-19 20:18:44 +0200

    Assets for 1.31

---
+++ updates/21/asset_collections_mappings_updates.sql
@@ -0,0 +1,22 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (102, "eip155:10/erc20:0xFdb794692724153d1488CcdBE0C56c252596735F");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (234, "eip155:10/erc20:0xdFA46478F9e5EA86d57387849598dbFB2e964b02");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (234, "eip155:1/erc20:0x8D6CeBD76f18E1558D4DB88138e2DeFB3909fAD6");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (234, "eip155:100/erc20:0x3F56e0c36d275367b8C502090EDF38289b3dEa0d");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (234, "eip155:42161/erc20:0x3F56e0c36d275367b8C502090EDF38289b3dEa0d");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (235, "eip155:137/erc20:0x27842334C55c01DDFE81Bf687425F906816c5141");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (235, "eip155:1/erc20:0xB2492E97a68a6E4B9E9a11B99F6C42E5aCCD38c7");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (236, "eip155:1/erc20:0xB49fa25978abf9a248b8212Ab4b87277682301c0");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (236, "eip155:8453/erc20:0x703D57164CA270b0B330A87FD159CfEF1490c0a5");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (237, "eip155:1/erc20:0x839e71613f9aA06E5701CF6de63E303616B0DDE3");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (237, "eip155:25/erc20:0x2D03bECE6747ADC00E1a131BBA1469C15fD11e03");
+*
---
+++ updates/21/asset_collections_updates.sql
@@ -0,0 +1,8 @@
+INSERT INTO asset_collections(id, name, symbol) VALUES (234, "MIMATIC", "MAI");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (235, "Veloce", "VEXT");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (236, "Rai.Finance", "SOFI");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (237, "VVS", "VVS");
+*

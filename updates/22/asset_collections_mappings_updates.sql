0bfe2e8fd8a30e0035e1b76098d8568d8b05c424
OjusWiZard <ojuswimail@gmail.com>
2023-12-07 02:03:29 +0530

    feat: Add monerium for Polygon and Gnosis chain
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

---
+++ updates/22/asset_collections_mappings_updates.sql
@@ -0,0 +1,6 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (240, "eip155:1/erc20:0x3231Cb76718CDeF2155FC47b5286d82e6eDA273f");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (240, "eip155:137/erc20:0x18ec0A6E18E5bc3784fDd3a3634b31245ab704F6");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (240, "eip155:100/erc20:0xcB444e90D8198415266c6a2724b7900fb12FC56E");
+*

226dfb049932ca72b62946aa64920d341bd46114
Yabir Benchakhtir <yabirg@protonmail.com>
2024-01-16 20:27:54 +0100

    First round of assets for 1.32

--- updates/22/asset_collections_updates.sql
+++ updates/22/asset_collections_updates.sql
@@ -1,2 +1,14 @@
 INSERT INTO asset_collections(id, name, symbol) VALUES (240, "Monerium EUR", "EURe");
 *
+INSERT INTO asset_collections(id, name, symbol) VALUES (241, "Anchored Coins EUR", "AEUR");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (242, "STFX", "STFX");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (243, "Seamless", "SEAM");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (244, "Injective Token", "INJ");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (245, "CyberConnect", "CYBER");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (246, "Bonk", "Bonk");
+*

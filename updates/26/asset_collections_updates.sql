5df988a5c2393616a4174dc976f837278874069e
Mohit Sahoo <git@mohitsahoo.com>
2024-06-19 23:19:59 +0530

    Add assets for huobi

--- updates/26/asset_collections_updates.sql
+++ updates/26/asset_collections_updates.sql
@@ -24,3 +24,37 @@ INSERT INTO asset_collections(id, name, symbol) VALUES (313, "Staked USDe", "sUS
 *
 INSERT INTO asset_collections(id, name, symbol) VALUES (314, "Curve.Fi USD Stablecoin", "crvUSD");
 *
+INSERT INTO asset_collections(id, name, symbol) VALUES (315, "MagicCraft", "MCRT");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (316, "Hillstone.Finance", "HSF");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (317, "Propy", "PRO");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (318, "SPELLFIRE Token", "SPELLFIRE");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (319, "InsurAce", "INSUR");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (320, "Rainmaker Games", "RAIN");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (321, "Ambire Wallet", "WALLET");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (322, "MContent", "MCONTENT");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (323, "GroveC", "GRV");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (324, "Rabbit", "RAB");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (325, "Trace Network", "TRACE");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (326, "BSSB", "BSSB");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (327, "Ready to Fight", "RTF");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (328, "pNetwork Token", "PNT");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (329, "WeWay Token", "WWY");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (330, "Cobak Token", "CBK");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (331, "MetaGaming Guild", "MGG");
+*

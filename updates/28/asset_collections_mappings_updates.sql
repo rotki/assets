f8e76ead63a10956ae63e70bb097b3a4cadd8f22
OjusWiZard <ojuswimail@gmail.com>
2024-08-13 23:20:58 +0530

    Fix (Assets): Group natives with wraps
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

---
+++ updates/28/asset_collections_mappings_updates.sql
@@ -0,0 +1,22 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (40, "BTC");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (38, "ETH");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (52, "eip155:1/erc20:0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (23, "XDAI");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (23, "eip155:100/erc20:0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (155, "LUNA-3");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (333, "eip155:10/erc20:0x2dAD3a13ef0C6366220f989157009e501e7938F8");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (333, "eip155:8453/erc20:0x2dAD3a13ef0C6366220f989157009e501e7938F8");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (334, "WELL");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (334, "eip155:8453/erc20:0xFF8adeC2221f9f4D8dfbAFa6B9a297d17603493D");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (334, "eip155:8453/erc20:0xA88594D404727625A9437C3f886C7643872296AE");
+*
---
+++ updates/28/asset_collections_updates.sql
@@ -0,0 +1,6 @@
+UPDATE asset_collections SET name='ETH', symbol='ETH' WHERE id=38;
+INSERT INTO asset_collections(id, name, symbol) VALUES (38, 'ETH', 'ETH');
+INSERT INTO asset_collections(id, name, symbol) VALUES (333, "Extra Finance", "EXTRA");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (334, "Moonwell", "WELL");
+*

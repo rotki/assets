c6102782c25d9729837ddc2ce5405165daa8bc83
Yabir Benchakhtir <yabirg@protonmail.com>
2024-11-01 13:31:35 +0100

    Assets frome exchanges

--- updates/30/asset_collections_mappings_updates.sql
+++ updates/30/asset_collections_mappings_updates.sql
@@ -14,3 +14,25 @@ INSERT INTO multiasset_mappings(collection_id, asset) VALUES (20, "eip155:8453/e
 *
 INSERT INTO multiasset_mappings(collection_id, asset) VALUES (20, "eip155:250/erc20:0x1E4F97b9f9F913c46F1632781732927B9019C68b");
 *
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (339, "eip155:1/erc20:0x0001A500A6B18995B03f44bb040A5fFc28E45CB0");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (339, "eip155:100/erc20:0xcE11e14225575945b8E6Dc0D4F2dD4C570f79d9f");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (339, "eip155:137/erc20:0xFEF5d947472e72Efbb2E388c730B7428406F2F95");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (340, "eip155:1/erc20:0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (340, "eip155:8453/erc20:0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:1/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:8453/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:42161/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:137/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:10/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, "eip155:43114/erc20:0x6985884C4392D348587B19cb9eAAf157F13271cd");
+*

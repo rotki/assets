7918ebaa48333d93da2060bad94bccec362708e9
Isaac Adewumi <ienioladewumi@gmail.com>
2024-11-27 16:03:39 +0100

    add morpho, walletconnect and cbtc assets

---
+++ updates/31/asset_collections_mappings_updates.sql
@@ -0,0 +1,6 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (40, "eip155:42161/erc20:0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (342, "eip155:8453/erc20:0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (342, "eip155:1/erc20:0x58D97B57BB95320F9a05dC918Aef65434969c2B2");
+*
---
+++ updates/31/asset_collections_updates.sql
@@ -0,0 +1,2 @@
+INSERT INTO asset_collections(id, name, symbol) VALUES (342, "Morpho", "MORPHO");
+*

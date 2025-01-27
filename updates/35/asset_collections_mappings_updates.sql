8f4b00dc2cfe06fa99e8e0f41fdb72e01a44052a
Yabir Benchakhtir <yabirg@protonmail.com>
2025-01-27 20:55:36 +0100

    New assets mappings 01/2025

---
+++ updates/35/asset_collections_mappings_updates.sql
@@ -0,0 +1,27 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:1/erc20:0xb01dd87B29d187F3E3a4Bf6cdAebfb97F3D9aB98');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:10/erc20:0x087C440F251Ff6Cfe62B86DdE1bE558B95b4bb9b');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:534352/erc20:0x087C440F251Ff6Cfe62B86DdE1bE558B95b4bb9b');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:42161/erc20:0x087C440F251Ff6Cfe62B86DdE1bE558B95b4bb9b');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:8453/erc20:0x087C440F251Ff6Cfe62B86DdE1bE558B95b4bb9b');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (349, 'eip155:43114/erc20:0x087C440F251Ff6Cfe62B86DdE1bE558B95b4bb9b');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (350, 'eip155:1/erc20:0xE0f63A424a4439cBE457D80E4f4b51aD25b2c56C');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (350, 'eip155:8453/erc20:0x50dA645f148798F68EF2d7dB7C1CB22A6819bb2C');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, 'eip155:1/erc20:0xeD35af169aF46a02eE13b9d79Eb57d6D68C1749e');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (341, 'eip155:8453/erc20:0x3792DBDD07e87413247DF995e692806aa13D3299');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (352, 'eip155:1/erc20:0x4DC26fC5854e7648a064a4ABD590bBE71724C277');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (352, 'eip155:42161/erc20:0x37a645648dF29205C6261289983FB04ECD70b4B3');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (353, 'eip155:1/erc20:0xC71B5F631354BE6853eFe9C3Ab6b9590F8302e81');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (353, 'eip155:56/erc20:0xC71B5F631354BE6853eFe9C3Ab6b9590F8302e81');

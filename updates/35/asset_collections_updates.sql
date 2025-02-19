b23f3019f56d833b3830d3cca2f5ca6c54a1dbb9
Isaac Adewumi <ienioladewumi@gmail.com>
2025-02-19 22:41:51 +0100

    add poloniex assets

--- updates/35/asset_collections_updates.sql
+++ updates/35/asset_collections_updates.sql
@@ -90,6 +90,78 @@ INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (393, 'XYRO',
 *
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (394, 'Shieldeum', 'SDM', 'eip155:56/erc20:0x516f8a1fB458EBdcFd0F544fF85c69C1c0Ebc31d');
 *
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (395, 'Alpaca Finance', 'ALPACA', 'eip155:56/erc20:0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (396, 'ApeCoin', 'APE', 'eip155:1/erc20:0x4d224452801ACEd8B2F0aebE155379bb5D594381');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (397, 'AstroPepeX', 'APX', 'eip155:1/erc20:0xed4e879087ebD0e8A77d66870012B5e0dffd0Fa4');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (398, 'Baby Doge Coin', 'BABYDOGE', 'eip155:1/erc20:0xAC57De9C1A09FeC648E93EB98875B212DB0d460B');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (399, 'Band Protocol', 'BAND', 'eip155:1/erc20:0xBA11D00c5f74255f56a5E366F4F77f5A186d7f55');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (400, 'BarnBridge', 'BOND', 'eip155:1/erc20:0x0391D2021f89DC339F60Fff84546EA23E337750f');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (401, 'Bitgert', 'BRISE', 'eip155:1/erc20:0xF2B2f7b47715256Ce4eA43363a867fdce9353e3A');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (402, 'Braintrust', 'BTRST', 'eip155:1/erc20:0x799ebfABE77a6E34311eeEe9825190B9ECe32824');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (403, 'BitTorrent', 'BTT', 'eip155:1/erc20:0xC669928185DbCE49d2230CC9B0979BE6DC797957');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (404, 'Byte', 'BYTE', 'eip155:1/erc20:0xdE342A3E269056fc3305f9e315F4C40D917BA521');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (405, 'Chromia', 'CHR', 'eip155:1/erc20:0x8A2279d4A90B6fe1C4B30fa660cC9f926797bAA2');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (406, 'DUSK', 'DUSK', 'eip155:1/erc20:0x940a2dB1B7008B6C776d4faaCa729d6d4A4AA551');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (407, 'Dypius', 'DYP', 'eip155:1/erc20:0x39b46B212bDF15b42B166779b9d1787A68b9D0c3');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (408, 'Step App', 'FITFI', 'eip155:43114/erc20:0x714f020C54cc9D104B6F4f6998C63ce2a31D1888');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (409, 'Gains Network', 'GNS', 'eip155:42161/erc20:0x18c11FD286C5EC11c3b683Caa813B77f5163A122');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (410, 'Keep3rV1', 'KP3R', 'eip155:1/erc20:0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (411, 'Layer3', 'L3', 'eip155:1/erc20:0x88909D489678dD17aA6D9609F89B0419Bf78FD9a');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (412, 'LimeWire', 'LMWR', 'eip155:1/erc20:0x628A3b2E302C7e896AcC432D2d0dD22B6cb9bc88');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (413, 'Liquity', 'LQTY', 'eip155:1/erc20:0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (414, 'Heroes of Mavia', 'MAVIA', 'eip155:1/erc20:0x24fcFC492C1393274B6bcd568ac9e225BEc93584');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (415, 'Mobox', 'MBOX', 'eip155:42161/erc20:0xdA661fa59320B808c5a6d23579fCfEdf1FD3cf36');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (416, 'Mog Coin', 'MOG', 'eip155:1/erc20:0xaaeE1A9723aaDB7afA2810263653A34bA2C21C7a');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (417, 'APENFT', 'NFT', 'eip155:1/erc20:0x198d14F2Ad9CE69E76ea330B374DE4957C3F850a');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (418, 'Non-Playable Coin', 'NPC', 'eip155:1/erc20:0x8eD97a637A790Be1feff5e888d43629dc05408F6');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (419, 'Pepe', 'PEPE', 'eip155:1/erc20:0x6982508145454Ce325dDbE47a25d4ec3d2311933');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (420, 'Planet Token', 'PLANET', 'eip155:1/erc20:0x2aD9adDD0d97EC3cDBA27F92bF6077893b76Ab0b');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (421, 'Radio Caca', 'RACA', 'eip155:1/erc20:0x12BB890508c125661E03b09EC06E404bc9289040');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (422, 'Radiant Capital', 'RDNT', 'eip155:1/erc20:0x137dDB47Ee24EaA998a535Ab00378d6BFa84F893');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (423, 'Rocket Pool', 'RPL', 'eip155:1/erc20:0xD33526068D116cE69F19A9ee46F0bd304F21A51f');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (424, 'Real Smurf Cat', 'SMURFCAT', 'eip155:1/erc20:0xfF836A5821E69066c87E268bC51b849FaB94240C');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (425, 'SWFTCOIN', 'SWFTC', 'eip155:1/erc20:0x0bb217E40F8a5Cb79Adf04E1aAb60E5abd0dfC1e');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (426, 'Tellor Tributes', 'TRB', 'eip155:1/erc20:0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (427, 'Volt Inu', 'VOLT', 'eip155:1/erc20:0x7f792db54B0e580Cdc755178443f0430Cf799aCa');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (428, 'Onyxcoin', 'XCN', 'eip155:1/erc20:0xA2cd3D43c775978A96BdBf12d733D5A1ED94fb18');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (429, 'XEN Crypto', 'XEN', 'eip155:1/erc20:0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (430, 'Venus', 'XVS', 'eip155:1/erc20:0xd3CC9d8f3689B83c91b7B59cAB4946B063EB894A');
+*
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (458, 'SOVRUN', 'SOVRN', 'eip155:1/erc20:0x031B8d752d73d7Fe9678ACEf26e818280D0646b4');
 *
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (459, 'AVL', 'AVL', 'eip155:1/erc20:0x5c8D0C48810FD37A0A824D074ee290E64f7A8Fa2');

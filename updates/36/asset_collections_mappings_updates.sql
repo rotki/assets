856410c529648a8067411d67550ade24d932be37
Yabir Benchakhtir <yabirg@protonmail.com>
2025-03-07 18:34:12 +0100

    Update of mappings

---
+++ updates/36/asset_collections_mappings_updates.sql
@@ -0,0 +1,4 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (470, 'eip155:56/erc20:0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (470, 'eip155:1/erc20:0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7');
+*
---
+++ updates/36/asset_collections_updates.sql
@@ -0,0 +1,2 @@
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (470, 'EURITE', 'EURI', 'eip155:1/erc20:0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7');
+*

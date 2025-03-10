c54cab6cf65be5f1363878181b1eaf72bbc86e3a
Yabir Benchakhtir <yabirg@protonmail.com>
2025-03-10 13:55:47 +0100

    Add collection for cbeth and more assets

--- updates/36/asset_collections_mappings_updates.sql
+++ updates/36/asset_collections_mappings_updates.sql
@@ -2,3 +2,13 @@ INSERT INTO multiasset_mappings(collection_id, asset) VALUES (470, 'eip155:56/er
 *
 INSERT INTO multiasset_mappings(collection_id, asset) VALUES (470, 'eip155:1/erc20:0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7');
 *
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (471, 'eip155:1/erc20:0xBe9895146f7AF43049ca1c1AE358B0541Ea49704');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (471, 'eip155:10/erc20:0xadDb6A0412DE1BA0F936DCaeb8Aaa24578dcF3B2');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (471, 'eip155:42161/erc20:0x1DEBd73E752bEaF79865Fd6446b0c970EaE7732f');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (471, 'eip155:137/erc20:0x4b4327dB1600B8B1440163F667e199CEf35385f5');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (471, 'eip155:8453/erc20:0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22');
+*
--- updates/36/asset_collections_updates.sql
+++ updates/36/asset_collections_updates.sql
@@ -1,2 +1,4 @@
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (470, 'EURITE', 'EURI', 'eip155:1/erc20:0x9d1A7A3191102e9F900Faa10540837ba84dCBAE7');
 *
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (471, "Coinbase Wrapped Staked ETH", "cbETH", "eip155:1/erc20:0xBe9895146f7AF43049ca1c1AE358B0541Ea49704");
+*

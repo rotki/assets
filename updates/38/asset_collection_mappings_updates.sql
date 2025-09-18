b8a1faab93a0d98557decf6455ed8c1a75ba9942
Isaac Adewumi <ienioladewumi@gmail.com>
2025-09-18 11:13:30 +0100

    fix beefy tokens

--- updates/38/asset_collection_mappings_updates.sql
+++ updates/38/asset_collection_mappings_updates.sql
@@ -1,12 +1,6 @@
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:1/erc20:0xB1F1ee126e9c96231Cc3d3fAD7C08b4cf873b1f1');
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (203, 'eip155:1/erc20:0xB1F1ee126e9c96231Cc3d3fAD7C08b4cf873b1f1');
 *
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:137/erc20:0xFbdd194376de19a88118e84E279b977f165d01b8');
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (203, 'eip155:56/erc20:0xCa3F508B8e4Dd382eE878A314789373D80A5190A');
 *
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:42161/erc20:0x99C409E5f62E4bd2AC142f17caFb6810B8F0BAAE');
-*
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:43114/erc20:0xd6070ae98b8069de6B494332d1A1a81B6179D960');
-*
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:56/erc20:0xCa3F508B8e4Dd382eE878A314789373D80A5190A');
-*
-INSERT INTO multiasset_mappings(collection_id, asset) VALUES (508, 'eip155:10/erc20:0x4E720DD3Ac5CFe1e1fbDE4935f386Bb1C66F4642');
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (203, 'eip155:10/erc20:0x4E720DD3Ac5CFe1e1fbDE4935f386Bb1C66F4642');
 *
--- updates/38/asset_collections_updates.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (508, 'Beefy', 'BIFI', 'eip155:1/erc20:0xB1F1ee126e9c96231Cc3d3fAD7C08b4cf873b1f1');
-*

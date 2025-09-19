cb55c79a4f6e35a784c632e55efe2b0ce25c9986
Isaac Adewumi <ienioladewumi@gmail.com>
2025-09-19 08:47:12 +0100

    adjust min/max versions for assets

--- validator/checker.py
+++ validator/checker.py
@@ -143,6 +143,7 @@ class UpdateChecker:
             11: REGEX_ASSETS_V3,
             12: REGEX_ASSETS_V3,
             13: REGEX_ASSETS_V3,
+            14: REGEX_ASSETS_V3,
         }
         self.string_re = re.compile(r'.*([\'"])(.*?)\1.*')
         self.test_version = 2

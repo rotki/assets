80a9327d5506ecc424cca33472b90d52cb20f7e6
Nicholas Yoder <nicholasyoder@proton.me>
2025-10-20 17:05:41 -0400

    Add binance assets

--- updates/38/asset_collections_updates.sql
+++ updates/38/asset_collections_updates.sql
@@ -10,3 +10,21 @@ INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (512, 'Solana
 *
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (513, 'World Liberty Financial', 'WLFI', 'eip155:1/erc20:0xdA5e1988097297dCdc1f90D4dFE7909e847CBeF6');
 *
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (514, '0G', '0G', 'eip155:1/erc20:0x4B948d64dE1F71fCd12fB586f4c776421a35b3eE');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (515, 'Lombard', 'BARD', 'eip155:1/erc20:0xf0DB65D17e30a966C2ae6A21f6BBA71cea6e9754');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (516, 'Falcon Finance', 'FF', 'eip155:1/erc20:0xFA1C09fC8B491B6A4d3Ff53A10CAd29381b3F949');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (517, 'Hemi', 'HEMI', 'eip155:1/erc20:0xEb964A1A6fAB73b8c72A0D15c7337fA4804F484d');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (518, 'Holoworld', 'HOLO', 'eip155:56/erc20:0x1a5D7E4c3A7F940B240b7357a4bFED30D17f9497');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (519, 'Linea', 'LINEA', 'eip155:1/erc20:0x1789e0043623282D5DCc7F213d703C6D8BAfBB04');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (520, 'Mira', 'MIRA', 'eip155:8453/erc20:0x7AaFD31a321d3627b30A8e2171264B56852187fe');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (521, 'Open', 'OPEN', 'eip155:1/erc20:0xA227Cc36938f0c9E09CE0e64dfab226cad739447');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (522, 'ZK Coin', 'ZKC', 'eip155:1/erc20:0x000006c2A22ff4A44ff1f5d0F2ed65F781F55555');
+*

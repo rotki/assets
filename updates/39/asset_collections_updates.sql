77d7cfd1cd3946590e46a54b6d0402cd02469fe0
Nicholas Yoder <145849440+nicholasyoder@users.noreply.github.com>
2026-01-06 05:52:22 -0800

    Add assets from bybit (#304)

---
+++ updates/39/asset_collections_updates.sql
@@ -0,0 +1,16 @@
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (547, 'Pieverse Token', 'PIEVERSE', 'eip155:1/erc20:0x0E63B9C287E32A05E6b9AB8ee8dF88A2760225A9');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (548, 'Lit Protocol', 'LITKEY', 'eip155:1/erc20:0x4D4eb0E8B160f6EbF63cC6d36060ffec09301B42');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (549, 'elizaOS', 'elizaOS', 'eip155:1/erc20:0xea17Df5Cf6D172224892B5477A16ACb111182478');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (550, 'Almanak', 'ALMANAK', 'eip155:1/erc20:0xDeFA1D21c5F1cbeac00eeB54B44C7D86467cc3a3');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (551, 'GAIB', 'GAIB', 'eip155:1/erc20:0xC19D38925F9F645337B1D1f37bAf3C0647A48E50');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (552, 'VOOI', 'VOOI', 'eip155:1/erc20:0xb31561F0e2aaC72406103b1926356D756F07A481');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (553, 'zkPass', 'ZKP', 'eip155:1/erc20:0xe1Be424F442D0687129128C6c38aAce44F8c8Dbc');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (554, 'COMMON', 'COMMON', 'eip155:8453/erc20:0x4c87da04887a1F9F21F777E3A8dD55C3C9f84701');
+*

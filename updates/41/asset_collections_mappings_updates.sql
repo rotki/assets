30caeb48bd2648bbe948545790eb796645884c6e
Yabir Benchakhtir <git@yabirgb.com>
2026-08-04 10:51:53 +0200

    More assets

--- updates/41/asset_collections_mappings_updates.sql
+++ updates/41/asset_collections_mappings_updates.sql
@@ -36,3 +36,45 @@ INSERT INTO multiasset_mappings(collection_id, asset) VALUES (607, 'solana/token
 *
 INSERT INTO multiasset_mappings(collection_id, asset) VALUES (607, 'eip155:56/erc20:0xd5f6ef5dEabE61E6d5CDB49BFB6f156F2c1cA715');
 *
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (608, 'solana/token:BMrgsEdrqfP4EAhiRbK5fFkq8xgQrqDAcZrv8R2pG4zG');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (608, 'eip155:1/erc20:0xFC347c996BD66C1d92E2045c80b413ef3fc84A90');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (609, 'eip155:1/erc20:0xAD29F2723fcdBcF665F210F25E06f97477e417cF');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (609, 'eip155:56/erc20:0x46F2564E0FA8248d15125E7e54173cfbdEf91Be7');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (610, 'eip155:1/erc20:0x68fa48B1C2FE52b3D776E1953e0E782b5044Ce28');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (610, 'eip155:56/erc20:0x68fa48B1C2FE52b3D776E1953e0E782b5044Ce28');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (610, 'solana/token:Xs3oZwbHvqis4NYcf4YKWmEia2eC84wSiVrcYcTqpH8');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (610, 'eip155:42161/erc20:0x68fa48B1C2FE52b3D776E1953e0E782b5044Ce28');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (611, 'eip155:1/erc20:0xD6C7bB8531295e88D364EA67D5D1acC7D3F87454');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (611, 'eip155:43114/erc20:0x61f23250451305f6C4426E81C50aE535Edf94A02');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (612, 'solana/token:CWZ6BsdnjkDVTGkmL6bGbJXXig6ceef12KvyGQW14cMt');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (612, 'eip155:56/erc20:0x6CEd5c6d3F913b48D59Fa07AbBfe9060C409087C');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (613, 'eip155:1/erc20:0x2103E845C5E135493Bb6c2A4f0B8651956eA8682');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (613, 'solana/token:5aLhp9VnUEKcsdtkfsf2DUgpJfomx7GmYVny24dHUZoB');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (613, 'eip155:56/erc20:0x23AE4fd8E7844cdBc97775496eBd0E8248656028');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (613, 'eip155:137/erc20:0xA7E22972a19dd924aFeEDf3Db28033B146801081');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (614, 'eip155:56/erc20:0x5506599c722389A60580B5213ea1Da60D64754a1');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (614, 'eip155:1/erc20:0xf326C8c9A691319f6330e27aa4A39F2d22C88dC5');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (614, 'eip155:8453/erc20:0x7297968FFb753dd12e4f6B1f18d9865c76707FC2');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (615, 'eip155:137/erc20:0xAC0F66379A6d7801D7726d5a943356A172549Adb');
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (615, 'solana/token:7JA5eZdCzztSfQbJvS8aVVxMFfd81Rs9VvwnocV1mKHu');
+*
--- updates/41/asset_collections_updates.sql
+++ updates/41/asset_collections_updates.sql
@@ -10,3 +10,19 @@ INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (606, 'Everyt
 *
 INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (607, 'Arcium', 'ARX', 'solana/token:ARXwZkNAtzPfdcoqQiduJn8EPv9fKiDfGn2KyggyDrFs');
 *
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (608, 'Zand AED', 'AEDZ', 'eip155:1/erc20:0xFC347c996BD66C1d92E2045c80b413ef3fc84A90');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (609, 'GRVT Token', 'GRVT', 'eip155:1/erc20:0xAD29F2723fcdBcF665F210F25E06f97477e417cF');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (610, 'SpaceX xStock', 'SPCXX', 'eip155:1/erc20:0x68fa48B1C2FE52b3D776E1953e0E782b5044Ce28');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (611, 'Fautor', 'FTR', 'eip155:1/erc20:0xD6C7bB8531295e88D364EA67D5D1acC7D3F87454');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (612, 'AntFun', 'ANTFUN', 'solana/token:CWZ6BsdnjkDVTGkmL6bGbJXXig6ceef12KvyGQW14cMt');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (613, 'Matrixdock Gold', 'XAUM', 'eip155:1/erc20:0x2103E845C5E135493Bb6c2A4f0B8651956eA8682');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (614, 'Zest Protocol', 'ZEST', 'eip155:1/erc20:0xf326C8c9A691319f6330e27aa4A39F2d22C88dC5');
+*
+INSERT INTO asset_collections(id, name, symbol, main_asset) VALUES (615, 'Geodnet', 'GEOD', 'eip155:137/erc20:0xAC0F66379A6d7801D7726d5a943356A172549Adb');
+*

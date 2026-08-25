9bef954ac411972176a9024b1cf53ff8956c8592
Yabir Benchakhtir <git@yabirgb.com>
2026-08-25 12:39:10 +0200

    Add sonic

---
+++ updates/42/updates.sql
@@ -0,0 +1,2 @@
+INSERT INTO assets(identifier, name, type) VALUES('S', 'Sonic', 'B'); INSERT INTO common_asset_details(identifier, symbol, coingecko, cryptocompare, forked, started, swapped_for) VALUES('S', 'S', 'sonic', 'SONIC', NULL, 1733011200, NULL);
+*
